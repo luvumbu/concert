be9f57900cb278257672ec1c844f0698edeb55ae
Luvumbu <luvumbu.n@gmail.com>
2020-08-12 20:55:51 +0200

    concert finis

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head id="head">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{title}}</title>
+</head>
+<body>
+
+<?php 
+	include("app.php");
+	include("link.php");
+?>
+ <style>
+.villes{
+	background-color: white;
+	color: black;
+	width: 300px;
+	text-align: center;
+}
+.villes img {
+	width: 100%;
+}
+#footer0{
+	width: 30%;
+	margin: auto;
+	font-size: 1.3em;
+	margin-top: 50px;
+
+}
+#footer0 i {
+	margin: 20px;
+}
+ </style>
+</body>
+</html>
---
+++ vue.js
@@ -0,0 +1,85 @@
+var head = new Vue({
+  el: '#head',
+  data: {    
+    title:"Bokonzi"
+  }
+})
+
+var header = new Vue({
+  el: '#header',
+  data: {
+    header1:"HOME",
+    header2:"BAND",
+    header3:"TOUR",
+    header4:"CONTACT",
+    header5:"MORE",
+    header6:"SEARCH",
+  }
+})
+
+var section = new Vue({
+  el: '#section',
+  data: {
+    section1_id:"section1_id",
+    section_style1:"section_style1",
+    section2_id:"section2_id",
+    section_style2:"section_style2",
+    section1:"New York",
+    section1_1:"The atmosphere in New York is lorem ipsum.",
+    section2:"Los Angeles",
+    section2_1:"We had the best time playing at Venice Beach!",
+    section3:"Chicago",
+    section3_1:"Thank you, Chicago - A night we won't forget.",
+    section_text1:"THE BAND",
+    section_text2:"We love music",
+    section_text3:"We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    section_text4:"Name",
+    section_text5:"Praesent tincidunt sed tellus ut rutrum sed vitae justo.",
+    section_picture1:"src/img/01.png",
+    section_picture1_class:"section_picture1_class",
+    section_picture2:"picture",
+    section_picture3:"picture",
+    section_picture4:"picture",
+    section_picture5:"picture",
+    section_text6:"TOUR DATES",
+    section_text7:"Remember to book your tickets!",
+    dates_tour1:"September",
+    dates_tour2:"October",
+    dates_tour3:"November",
+    dates_statue_off:"Sold out",
+    dates_statue_on:"Sold ok",
+    citys_name1:"New York",
+    citys_name2:"Paris",
+    citys_name3:"San Francisco",
+    section_dates1:"",
+    section_dates2:"",
+    section_dates3:"",
+    contact_number:"Phone: +00 151515",
+    contact_city:" Chicago, US",
+    contact_mail:"Email: mail@mail.com",
+    followme1:"",
+    followme2:"",
+    followme3:"",
+    followme4:"",
+    followme5:"",
+   
+    
+    
+
+
+
+    
+
+
+  }
+})
+
+var footer = new Vue({
+  el: '#footer',
+  data: {
+    
+  }
+})
+ 
+
+
